37aee3bff12209b5020a3ffe5a36ee0e1475f4a3
mansoura50 <adammansour600@gmail.com>
2019-04-25 11:22:40 -0400

    Castle and Camera Movement added
    At this point the assignment is completed. Everything works as intended although the camera can be a bit awkward at times.

---
+++ Assets/Scripts/FollowCam.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCam : MonoBehaviour {
+	static public GameObject POI;
+	[Header("Set in Inspector")]
+	public float easing = 0.05f;
+	public Vector3 minXY = Vector3.zero;
+
+	[Header("Set Dynamically")]
+	public float camZ;
+
+	void Awake () {
+		camZ = this.transform.position.z;
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	void FixedUpdate () {
+		//makes sure we know the POI
+		if (POI == null)
+			return;
+
+		//get the position of the projectile
+		Vector3 destination = POI.transform.position;
+		destination.x = Mathf.Max (minXY.x, destination.x);
+		destination.x = Mathf.Max (minXY.y, destination.y);
+
+
+		destination = Vector3.Lerp (transform.position, destination, easing);
+
+		destination.z = camZ;
+		//sets the camera to the destination
+		transform.position = destination;
+
+		//zoom of camera in relation to projectile
+		Camera.main.orthographicSize = destination.y + 10;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
